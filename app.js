5bb1bca1e6f08c07f781875ffaf12c6d6ffd8c84
umitishere <umitsancakli98@gmail.com>
2021-05-24 08:40:34 +0300

    Send https get request with weatherapp url

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
+
+app.get("/", (req, res) => {
+
+    // You need to add appid parameter from your openweathermap account to get the result.
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Istanbul&units=metric&appid=c48b651fd7b1b3bd091de9fd74605c55";
+
+    https.get(url, (response) => {
+        console.log(response.statusCode);
+    });
+
+    res.send("Server is running");
+
+});
 
 app.listen(3000, () => {
     console.log("Server is running.");
